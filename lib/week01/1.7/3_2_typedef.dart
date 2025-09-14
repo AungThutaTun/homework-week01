ee2fee4ae076a3a7d0e700c040e58d35b8d873e2
thuta98 <tripletthuta@gmail.com>
2025-09-15 02:34:26 +0900

    update something

---
+++ lib/week01/1.7/3_2_typedef.dart
@@ -0,0 +1,18 @@
+typedef Operation = void Function(int x, int y);
+
+void add(int x, int y) {
+  print('결괏값: ${x + y}');
+}
+
+void subtract(int x, int y) {
+  print('결괏값: ${x - y}');
+}
+
+void main() {
+  Operation oper = add;
+  oper(1,2);
+  oper = subtract;
+  oper(1,2);
+
+  
+}
