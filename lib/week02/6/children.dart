6b583d7772fa889740c3348e9c81e4e3fbc1a974
thuta98 <tripletthuta@gmail.com>
2025-09-21 23:36:47 +0900

    update something

---
+++ lib/week02/6/children.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: SizedBox(,
+        width: double.infinity,
+        child: Column(children: [MainAxisAlignment: MainAxisAlignment.Center,
+        
+        
+        children:[
+          Text('Code'),
+          Text('Factory'),
+        ]
+        ()],))
+      ),
+    ),
+  );
+}
