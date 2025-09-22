4528a4e36cef5e77c670c871485d493228afea4c
thuta98 <tripletthuta@gmail.com>
2025-09-22 09:18:18 +0900

    update something

---
+++ lib/week03/expanded_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ColumnWidgetExample());  
+}
+
+class ColumnWidgetExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+         width: double.infinity,
+         child: Column(
+         children:[
+          Expanded(
+          child:Container(
+            color:Colors.blue,
+
+          ),
+          ),
+          Expended(
+            child:Container(
+              color:Colors.red,
+          ),
+          ),
+         ],
+         ),
+        ),
+      ),
+    );
+  }
+}
