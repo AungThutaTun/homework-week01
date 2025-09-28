7d02a84d0c69c2e447f29092a1d02b9e35a37f59
thuta98 <tripletthuta@gmail.com>
2025-09-29 02:16:42 +0900

    update something

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,12 +10,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final PageController _pageController = PageController();
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _timer = Timer.periodic(const Duration(seconds: 3), (timer) {
+      int? nextPage = _pageController.page?.toInt();
+
+      if (nextPage == null) return;
+
+      if (nextPage == 4) {
+        nextPage = 0;
+      } else {
+        nextPage++;
+      }
+
+      _pageController.animateToPage(
+        nextPage,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeIn,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
 
     return Scaffold(
       body: PageView(
+        controller: _pageController,
         children: [1, 2, 3, 4, 5]
             .map(
               (number) => Image.asset(
