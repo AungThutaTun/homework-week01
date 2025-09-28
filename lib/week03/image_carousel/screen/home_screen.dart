2950c16e6c1132578857af82268cf7acc6072640
thuta98 <tripletthuta@gmail.com>
2025-09-29 01:49:54 +0900

    update something

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key : key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body:  Text('Home Screen'),
+      body: PageView(
+        children: [1, 2, 3, 4, 5]
+            .map((number) => Image.asset('asset/img/image_$number.jpeg'))
+            .toList(),
+      ),
     );
   }
 }
