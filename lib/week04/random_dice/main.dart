575d81eef239f71f440eeeb3eb9f58727ca5bd76
thuta98 <tripletthuta@gmail.com>
2025-09-29 03:02:52 +0900

    update something

---
+++ lib/week04/random_dice/main.dart
@@ -0,0 +1,11 @@
+import 'package:u_and_i/week04/random_dice/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(  
+    MaterialApp(  
+      home: HomeScreen(),
+    ),
+  );
+}
+
--- lib/week04/random_dice/screen/home_screen.dart
+++ lib/week04/random_dice/screen/home_screen.dart
@@ -1,53 +1,79 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
-  const HomeScreen({Key? key}) : super(key : key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){ 
-    return Scaffold(  
-     body :Text('Home Screen'),
-    );
-  }
- }
-
-
- class _DDay extends StatelessWidget{
-  @override
-  String toStringDeep({String prefixLineOne = '', String? prefixOtherLines, DiagnosticLevel minLevel = DiagnosticLevel.debug, int wrapWidth = 65}) {
-    // TODO: implement toStringDeep
-    return super.toStringDeep(prefixLineOne: prefixLineOne, prefixOtherLines: prefixOtherLines, minLevel: minLevel, wrapWidth: wrapWidth);
-  }Widget build (BuildContext context) {
-    return Text("DDay Widget");
-  }
- }
-
- class _CoupleImage extends StatelessWidget{
-  @override
-  Widget build (BuildContext context) {
-    return Text("Couple Image Widget");
-  }
- }
-
- class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key : key);
-
-  @override
-  Widget build(BuildContext context){ 
-    return Scaffold(  
-      body: SafeArea(  
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
         top: true,
         bottom: false,
-        child: Column(  
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
+          children: const [
             _DDay(),
             _CoupleImage(),
           ],
-        )
+        ),
       ),
     );
   }
- }
- 
+}
+
+class _DDay extends StatelessWidget {
+  const _DDay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        const Text(
+          'U&I',
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 16.0),
+        const Text(
+          '우리 처음 만날 날',
+          style: TextStyle(fontSize: 20),
+        ),
+        const Text(
+          '2021.11.23',
+          style: TextStyle(fontSize: 20),
+        ),
+        const SizedBox(height: 16.0),
+        IconButton(
+          iconSize: 60.0,
+          onPressed: () {},
+          icon: const Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
+        ),
+        const SizedBox(height: 16.0),
+        const Text(
+          'D+365',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
+
+class _CoupleImage extends StatelessWidget {
+  const _CoupleImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset(
+        'asset/img/middle_image.png', // <-- make sure this matches pubspec.yaml
+        height: MediaQuery.of(context).size.height / 2,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
